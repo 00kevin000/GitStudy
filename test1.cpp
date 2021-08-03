6d0ec16d93832ce011ee6ab5a7ffe0275e6e32ab
zhengkaiwen5 <zhengkaiwen5@xiaomi.com>
2021-08-03 20:03:33 +0800

    test use pull rebase
    Signed-off-by: zhengkaiwen5 <zhengkaiwen5@xiaomi.com>

--- test1.cpp
+++ test1.cpp
@@ -6,6 +6,6 @@ int main() {
     int i = 0;
     int i2 = 1;
     // I don't want to keep this message
-    // I don't want to keep this message
+    // I modify this message
     return 0; // master and linux all modify here
 }
