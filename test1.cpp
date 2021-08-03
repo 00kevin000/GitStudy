a0a9d2799a411489cf90d482ae3776c0f5a1bf08
zhengkaiwen5 <zhengkaiwen5@xiaomi.com>
2021-08-03 20:07:25 +0800

    linux modify something Signed-off-by: zhengkaiwen5 <zhengkaiwen5@xiaomi.com>

--- test1.cpp
+++ test1.cpp
@@ -5,7 +5,9 @@ using namespace std;
 int main() {
     int i = 0;
     int i2 = 1;
+    int j = 0;
     // I don't want to keep this message
-    // I don't want to keep this message
+    // I hhhh
+    // I don't know this message
     return 0; // linux modify here
 }
